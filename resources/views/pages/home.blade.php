3becaee7630e3611552d60b0aa52a25c3c7e8d6f
Marcel Stadelmann <m@marceli.to>
2024-10-18 18:20:55 +0200

    wip

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,23 +1,35 @@
 @extends('app')
 @section('content')
-    <img 
-      src="{{ asset('img/palimpsest_home_'.locale().'-tall.webp') }}" 
-      alt="{{ __(env('APP_NAME')) }}" 
-      class="w-full h-[calc(100dvh_-_78px)] lg:h-[calc(100dvh_-_114px)] object-cover object-right-top hidden square:hidden wide:hidden tall:block">
+<img 
+  src="{{ asset('img/palimpsest_home_'.locale().'-tall.webp') }}" 
+  alt="{{ __(env('APP_NAME')) }}" 
+  class="w-full h-[calc(100dvh_-_78px)] lg:h-[calc(100dvh_-_114px)] object-cover object-right-top hidden square:hidden wide:hidden tall:block">
 
-    <img 
-      src="{{ asset('img/palimpsest_home_'.locale().'-tall-md.webp') }}" 
-      alt="{{ __(env('APP_NAME')) }}" 
-      class="w-full h-[calc(100dvh_-_78px)] lg:h-[calc(100dvh_-_114px)] object-cover object-right-top hidden tall:hidden tall-md:block square:hidden wide:hidden">
+<img 
+  src="{{ asset('img/palimpsest_home_'.locale().'-tall-md.webp') }}" 
+  alt="{{ __(env('APP_NAME')) }}" 
+  class="w-full h-[calc(100dvh_-_78px)] lg:h-[calc(100dvh_-_114px)] object-cover object-right-top hidden tall:hidden tall-md:block square:hidden wide:hidden">
 
+  <picture>
+    <source srcset="{{ asset('img/palimpsest_home_'.locale().'-square.webp') }}" type="image/webp">
     <img 
-      src="{{ asset('img/palimpsest_home_'.locale().'-square.webp') }}" 
+      src="{{ asset('img/palimpsest_home_'.locale().'-square.jpg') }}') }}" 
       alt="{{ __(env('APP_NAME')) }}" 
+      title="{{ __(env('APP_NAME')) }}" 
+      height="2000" 
+      width="4000"
       class="w-full h-[calc(100dvh_-_78px)] lg:h-[calc(100dvh_-_114px)] object-cover object-right-top hidden tall:hidden wide:hidden square:block">
+  </picture>
 
-      <img 
-        src="{{ asset('img/palimpsest_home_'.locale().'-wide.webp') }}" 
-        alt="{{ __(env('APP_NAME')) }}" 
-        class="w-full h-[calc(100dvh_-_78px)] lg:h-[calc(100dvh_-_114px)] object-cover object-right-top hidden tall:hidden square:hidden wide:block">
+  <picture>
+    <source srcset="{{ asset('img/palimpsest_home_'.locale().'-wide.webp') }}" type="image/webp">
+    <img 
+      src="{{ asset('img/palimpsest_home_'.locale().'-wide.jpg') }}" 
+      alt="{{ __(env('APP_NAME')) }}" 
+      title="{{ __(env('APP_NAME')) }}" 
+      height="2000" 
+      width="4000"
+      class="w-full h-[calc(100dvh_-_78px)] lg:h-[calc(100dvh_-_114px)] object-cover object-right-top hidden tall:hidden square:hidden wide:block">
+  </picture>
 
 @endsection
