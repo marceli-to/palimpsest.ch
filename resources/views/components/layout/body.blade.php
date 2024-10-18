669d593aa97050abe8c929710bc454d863025e1c
Marcel Stadelmann <m@marceli.to>
2024-10-18 18:00:07 +0200

    wip

--- resources/views/components/layout/body.blade.php
+++ resources/views/components/layout/body.blade.php
@@ -1,5 +1,5 @@
 <body class="bg-royal antialiased font-medium leading-[1.125] lg:leading-[1.2] mx-auto flex flex-col min-h-dvh lg:min-h-screen">
-  <x-misc.debug />
+  {{-- <x-misc.debug /> --}}
   {{ $slot }}
   @vite('resources/js/app.js')
 </body>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,23 +1,23 @@
 @extends('app')
 @section('content')
     <img 
-      src="{{ asset('img/palimpsest_home_de-tall.webp') }}" 
-      alt="Palimpsest" 
-      class="w-full h-[calc(100dvh_-_78px)] lg:h-[calc(100dvh_-_114px)] object-cover object-right-top hidden tall:block square:hidden wide:hidden">
+      src="{{ asset('img/palimpsest_home_'.locale().'-tall.webp') }}" 
+      alt="{{ __(env('APP_NAME')) }}" 
+      class="w-full h-[calc(100dvh_-_78px)] lg:h-[calc(100dvh_-_114px)] object-cover object-right-top hidden square:hidden wide:hidden tall:block">
 
     <img 
-      src="{{ asset('img/palimpsest_home_de-tall-md.webp') }}" 
-      alt="Palimpsest" 
-      class="w-full h-[calc(100dvh_-_78px)] lg:h-[calc(100dvh_-_114px)] object-cover object-right-top hidden tall-md:block tall:hidden square:hidden wide:hidden">
+      src="{{ asset('img/palimpsest_home_'.locale().'-tall-md.webp') }}" 
+      alt="{{ __(env('APP_NAME')) }}" 
+      class="w-full h-[calc(100dvh_-_78px)] lg:h-[calc(100dvh_-_114px)] object-cover object-right-top hidden tall:hidden tall-md:block square:hidden wide:hidden">
 
     <img 
-      src="{{ asset('img/palimpsest_home_de-square.webp') }}" 
-      alt="Palimpsest" 
-      class="w-full h-[calc(100dvh_-_78px)] lg:h-[calc(100dvh_-_114px)] object-cover object-right-top hidden square:block tall:hidden wide:hidden">
+      src="{{ asset('img/palimpsest_home_'.locale().'-square.webp') }}" 
+      alt="{{ __(env('APP_NAME')) }}" 
+      class="w-full h-[calc(100dvh_-_78px)] lg:h-[calc(100dvh_-_114px)] object-cover object-right-top hidden tall:hidden wide:hidden square:block">
 
       <img 
-        src="{{ asset('img/palimpsest_home_de-wide.webp') }}" 
-        alt="Palimpsest" 
-        class="w-full h-[calc(100dvh_-_78px)] lg:h-[calc(100dvh_-_114px)] object-cover object-right-top hidden wide:block tall:hidden square:hidden">
+        src="{{ asset('img/palimpsest_home_'.locale().'-wide.webp') }}" 
+        alt="{{ __(env('APP_NAME')) }}" 
+        class="w-full h-[calc(100dvh_-_78px)] lg:h-[calc(100dvh_-_114px)] object-cover object-right-top hidden tall:hidden square:hidden wide:block">
 
 @endsection
